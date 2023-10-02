46e84aa0eba6231ab5bc9cca59fd161bc8d28fe0
Fernando Jurado <ffjla@dtu.dk>
2023-10-02 15:35:08 +0200

    Surrogate training script updated. Now working with a simple sample

--- tutorials/surrogate/training.py
+++ tutorials/surrogate/training.py
@@ -25,23 +25,26 @@ TUTORIALS_PATH = os.path.dirname(SELF_PATH)
 RESULTS_PATH = os.path.join(TUTORIALS_PATH, "results")
 
 # Lets create a dataloader
-BATCH_SIZE = 1024
+BATCH_SIZE = 256
 
 # Neural network parameters
 INPUT_SIZE = 203
-HIDDEN_SIZE = 256
+HIDDEN_SIZE = 128
 OUTPUT_SIZE = 99
-LEARNING_RATE = 1e-6
-NUM_EPOCHS = 400
-PRINT_EVERY = 10
+NUM_CLUSTERS = 100
+LEARNING_RATE = 1e-3
+NUM_EPOCHS = 500
+PRINT_EVERY = 50
 PLOT_EVERY = 1
 
 
 class NetworkDataset(Dataset):
-    def __init__(self, X, y):
-        self.X = torch.from_numpy(X.astype(np.float32))
-        self.y = torch.from_numpy(y.astype(np.float32))
-        self.len = self.X.shape[0]
+    def __init__(self, weights, current_membership, y_membership):
+        self.weights = torch.tensor(weights, dtype=torch.float32)
+        self.X = torch.tensor(
+            current_membership)
+        self.y = torch.tensor(y_membership)
+        self.len = len(self.weights)+len(self.X)
 
     def __len__(self):
         return self.len
@@ -50,21 +53,35 @@ class NetworkDataset(Dataset):
         return self.X[idx], self.y[idx]
 
 
-class NeuralNetwork(nn.Module):
-    def __init__(self, input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE, output_size=OUTPUT_SIZE):
-        super(NeuralNetwork, self).__init__()
-        self.layer_1 = nn.Linear(input_size, hidden_size)
-        self.layer_2 = nn.Linear(hidden_size, output_size)
-        # nn.init.kaiming_uniform_(self.layer_1.weight, nonlinearity='relu')
-        self.relu = nn.ReLU()
-        # self.sigmoid = nn.Sigmoid()
+class LSTM(nn.Module):
+    def __init__(self, embedding_dim, vocab_size, output_size, extra_input_size=0):
+        super(LSTM, self).__init__()
 
-    def forward(self, x):
-        x = self.layer_1(x)
-        x = self.relu(x)
-        x = self.layer_2(x)
-        # x = self.sigmoid(x)
-        return x
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(embedding_dim+extra_input_size,
+                            embedding_dim, batch_first=True)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2tag = nn.Linear(embedding_dim, output_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, sequence, extra_input=None):
+        embeds = self.embedding(sequence)
+        # Concatenate the extra input
+        if extra_input is not None:
+            num_rows = embeds.shape[0]
+            extra_input = extra_input.repeat(num_rows, 1)
+            embeds = torch.cat((embeds, extra_input), dim=-1)
+        # Lets get the lstm
+        output, (h, c) = self.lstm(embeds)
+        # Lets get the output
+        output = self.hidden2tag(output)
+        # Lets get the softmax
+        output = self.softmax(output)
+        return output, (h, c)
 
 
 def process_data():
@@ -82,12 +99,12 @@ def process_data():
             with open(os.path.join(RESULTS_PATH, file), "r") as f:
                 data[name] = json.load(f)
 
-    data_list = []
+    samples = {}
 
     for name, data in data.items():
         # How many rounds are there?
         max_rounds = len(data)
-        # print(f"Name: {name}")
+        samples[name] = {}
         # Lets loop through the rounds and print the alive nodes
         for round, stats in data.items():
             round = int(round)
@@ -97,200 +114,186 @@ def process_data():
             membership = []
             for node_id, energy in stats['energy_levels'].items():
                 energy_levels.append(energy)
-            # Scale the energy levels to 0 to 1, the largest number is 10 J
-            # energy_levels = [x / 10 for x in energy_levels]
-            # for node_id, cluster_head in stats['cluster_heads'].items():
-            #     cluster_heads.append(cluster_head)
             for node_id, cluster_id in stats['membership'].items():
-                membership.append(cluster_id)
-            # Remove the first element which is the sink
-            membership.pop(0)
-            # Replace None with 0
-            membership = [0 if x is None else x for x in membership]
-            # Scale the membership to 0 to 1, the largest number is 100
-            # membership = [x / 100 for x in membership]
-            # put the name as numbers as they are currently strings
-            row = [float(name[0]), float(name[1]), float(name[2]),
-                   float(round), float(stats['alive_nodes'])]
+                if cluster_id is None:
+                    membership.append(0)
+                else:
+                    membership.append(int(cluster_id))
+            # Add a zero for the sink
+            membership.insert(0, 0)
+            x_data = [float(name[0]), float(name[1]), float(name[2]),
+                      float(round), float(stats['alive_nodes'])]
             # append the energy levels
-            row.extend(energy_levels)
-            # append the membership
-            row.extend(membership)
-            # Y is the membership of the next round
+            x_data.extend(energy_levels)
             # Lets check if this is the last round
             if int(round) == max_rounds:
                 # If it is then the membership is the same as the current round
-                row.extend(membership)
+                y_data = membership
             else:
                 # Otherwise the membership is the next round
-                # Lets get the next round
                 next_round = int(round) + 1
                 # Lets get the membership of the next round
                 next_round_membership = []
                 for node_id, cluster_id in data[str(next_round)]['membership'].items():
-                    next_round_membership.append(cluster_id)
-                # Remove the first element which is the sink
-                next_round_membership.pop(0)
-                # Replace None with 0
-                next_round_membership = [
-                    0 if x is None else x for x in next_round_membership]
-                # Scale the membership to 0 to 1, the largest number is 100
-                # next_round_membership = [
-                #     x / 100 for x in next_round_membership]
-                row.extend(next_round_membership)
+                    if cluster_id is None:
+                        next_round_membership.append(0)
+                    else:
+                        # Attach only the cluster id in integer form, remove .0
+                        next_round_membership.append(int(cluster_id))
+                # Add a zero for the sink
+                next_round_membership.insert(0, 0)
+                y_data = next_round_membership
+            # Lets append the x_data and y_data to the samples
+            samples[name][round] = {
+                "x_data": x_data,
+                "membership": membership,
+                "y_data": y_data
+            }
 
-            data_list.append(row)
-
-    # Split the data into training and testing data
-    # Lets use 80% for training and 20% for testing
-    # Lets shuffle the data
-    random.shuffle(data_list)
-    # The X data is the whole data except the last 99 columns which are the membership
-    x_data = np.array(data_list)[:, :-99]
-    # The y data are the last 99 columns which are the membership
-    y_data = np.array(data_list)[:, -99:]
-
-    # Lets save the training and testing data in a json file
+    # Lets save the entire dict by name
     os.makedirs("data", exist_ok=True)
-    with open(os.path.join("data", "x_data.json"), "w") as f:
-        json.dump(x_data.tolist(), f)
-    with open(os.path.join("data", "y_data.json"), "w") as f:
-        json.dump(y_data.tolist(), f)
+    for name, data in samples.items():
+        # Lets save the x_data
+        with open(os.path.join("data", f"{name}.json"), "w") as f:
+            json.dump(data, f)
 
-    return x_data, y_data
+    return samples
+
+
+def load_samples():
+    samples = {}
+    for file in os.listdir("data"):
+        # Ignore .DS_Store
+        if file == ".DS_Store":
+            continue
+        # Load the data
+        with open(os.path.join("data", file), "r") as f:
+            data = json.load(f)
+        # Get the name only which is after LEACH-CE-E_ and before .json
+        # Lets get the name which is simply the concatenation of what is inside the ''
+        name = file.split(".json")[0]
+        name = name.replace("(", "").replace(")", "").split(",")
+        name = [x.replace("'", "") for x in name]
+        name = [float(x) for x in name]
+        name = tuple(name)
+        samples[name] = data
+    return samples
+
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def random_sample(samples):
+    random_key = random.choice(list(samples.keys()))
+    current_membership = []
+    y_membership = []
+    weights = []
+    sample = samples[random_key]
+    for round in range(1, len(sample)+1):
+        x_data = sample[str(round)]['x_data']
+        y_data = sample[str(round)]['y_data']
+        pre_membership = sample[str(round)]['membership']
+        weights.append(x_data)
+        current_membership.append(pre_membership)
+        y_membership.append(y_data)
+
+    return weights, current_membership, y_membership
+
+
+def get_sample_ch(ch, samples):
+    # Get a sample where there is a cluster head with id ch
+    X_train = []
+    y_train = []
+    for key, sample in samples.items():
+        for round in range(1, len(sample)+1):
+            cluster_heads = sample[str(round)]['y_data']
+            if ch in cluster_heads:
+                y_data = sample[str(round)]['y_data']
+                pre_membership = sample[str(round)]['membership']
+                x_data = pre_membership
+                X_train.append(x_data)
+                y_train.append(y_data)
+                return X_train, y_train
 
 
 def main(args):
     if args.data is None:
-        X_train, y_train = process_data()
+        samples = process_data()
     else:
         # Load the training and testing data
-        for file in os.listdir(args.data):
-            # Ignore .DS_Store
-            if file == ".DS_Store":
-                continue
-            if file == "x_data.json":
-                with open(os.path.join(args.data, file), "r") as f:
-                    X_train = np.array(json.load(f))
-            elif file == "y_data.json":
-                with open(os.path.join(args.data, file), "r") as f:
-                    y_train = np.array(json.load(f))
-            else:
-                print(f"File: {file} is not a valid file.")
-                sys.exit(1)
+        samples = load_samples()
 
-    # Lets one hot encode the y_train
-    encoder = OneHotEncoder(sparse=False).fit(y_train)
-    # Lets transform the y_train
-    y_train = encoder.transform(y_train)
+    print(f"Number of samples: {len(samples)}")
 
-    print(y_train.shape)
+    weights, current_membership, y_membership = random_sample(samples)
 
-    # Accessing the one-hot encoding mapping
-    categories = encoder.categories_
+    # Lets create the training data
+    Training_DataSet = NetworkDataset(weights=weights,
+                                      current_membership=current_membership,
+                                      y_membership=y_membership)
 
-    # Printing the one-hot encoding mapping
-    for i, category in enumerate(categories):
-        print(f'Category {i}: {category}')
+    # Lets create the dataloader
+    train_dataloader = DataLoader(
+        Training_DataSet, batch_size=1, shuffle=False)
 
-    # Split the training data into training and testing data
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_train, y_train, test_size=0.7, shuffle=True)
+    print(f"Lenght of the dataloader: {len(train_dataloader)}")
 
-    # Lets load the training data
-    train_dataset = NetworkDataset(X_train, y_train)
-    train_loader = DataLoader(dataset=train_dataset,
-                              batch_size=BATCH_SIZE, shuffle=True)
+    print(
+        f"Shape of the training data, x: {Training_DataSet.X.shape}, {Training_DataSet.X.dtype}, y: {Training_DataSet.y.shape}, {Training_DataSet.y.dtype}")
 
-    # Lets load the testing data
-    test_dataset = NetworkDataset(X_test, y_test)
-    test_loader = DataLoader(dataset=test_dataset,
-                             batch_size=BATCH_SIZE, shuffle=True)
+    model = LSTM(embedding_dim=120, vocab_size=101, output_size=101,
+                 extra_input_size=Training_DataSet.weights.shape[1])
 
-    # Check it's working
-    for batch, (X, y) in enumerate(train_loader):
-        print(f"Batch: {batch+1}")
-        print(f"X shape: {X.shape}")
-        print(f"y shape: {y.shape}")
-        break
+    criterion = nn.CrossEntropyLoss()
 
-    # Lets create the neural network
-    model = NeuralNetwork(INPUT_SIZE, HIDDEN_SIZE, y_train.shape[1])
-
-    # Loss function
-    criterion = nn.MSELoss()
-
-    # Optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-    print(model)
-
-    # Lets train the neural network
-    # Keep track of losses for plotting
-    loss_values = []
-
-    start_time = time.time()
-
+    # Lets train with the Training_DataSet
+    loss_list = []
+    model.train()
+    # i = 0
     for epoch in range(NUM_EPOCHS):
-        model.train()
-        for seq, y in train_loader:
-            optimizer.zero_grad()
+        X = Training_DataSet.X[0]
+        y = Training_DataSet.y[0]
+        weights = Training_DataSet.weights[0]
+        model.zero_grad()
+        output, (h, c) = model(X, weights)
 
-            y_pred = model(seq)
+        loss = criterion(output, y)
+        loss_list.append(loss.item())
 
-            # print(f"y_pred = {y_pred}")
-            # input(f"y_train = {y_train}")
+        loss.backward()
+        optimizer.step()
 
-            loss = criterion(y_pred, y)
-            loss_values.append(loss.item())
-            loss.backward()
-            optimizer.step()
-
-        print(f'Epoch: {epoch+1:2} Loss: {loss.item():10.8f}')
-
-        # Validation
         if epoch % PRINT_EVERY != 0:
             continue
-        model.eval()
-        with torch.no_grad():
-            y_pred = model(train_dataset.X)
-            train_mse = np.sqrt(criterion(y_pred, train_dataset.y).item())
-            y_pred = model(test_dataset.X)
-            test_mse = np.sqrt(criterion(y_pred, test_dataset.y).item())
-        print(
-            f"Epoch: {epoch+1:2} Train RMSE: {train_mse:10.8f} Test RMSE: {test_mse:10.8f}")
 
-    print(f'\nDuration: {time.time() - start_time:.0f} seconds')
+        print(f"EPOCH: {epoch}, loss: {loss.item()}")
 
-    print("Training Complete")
+        _, preds = output.max(dim=-1)
 
-    step = np.linspace(0, 100, len(loss_values))
+        # Check how many are correct
+        correct = 0
+        total = 0
+        for i in range(len(preds)):
+            if preds[i] == y[i]:
+                correct += 1
+            total += 1
+        print(f"Correct: {correct}")
+        print(f"Accuracy: {correct/total}")
 
-    fig, ax = plt.subplots(figsize=(8, 5))
-    plt.plot(step, np.array(loss_values))
-    plt.title("Step-wise Loss")
-    plt.xlabel("Epochs")
+    # Lets plot the loss
+    plt.figure()
+    plt.plot(loss_list)
+    plt.xlabel("Epoch")
     plt.ylabel("Loss")
+    plt.title("Loss")
     plt.show()
-
-    # Lets find out the accuracy of the model
-    # Lets loop through the test data
-    model.eval()
-    accuracy = 0
-    total = 0
-    with torch.no_grad():
-        for seq, y_test in test_loader:
-            y_pred = model(seq)
-            # Inverse transform the predicted one-hot encoded labels to get categories
-            y_pred = encoder.inverse_transform(y_pred)
-            print(f"y_pred: {y_pred}")
-            # Inverse transform the actual one-hot encoded labels to get categories
-            y_test = encoder.inverse_transform(y_test)
-            print(f"y_test: {y_test}")
-            total += y_test.size(0)
-            accuracy += (y_pred == y_test).sum().item()
-
-    print(f"Accuracy: {accuracy / total * 100} %")
 
 
 if __name__ == "__main__":
