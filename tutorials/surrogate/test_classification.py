7eb9e3bd73dca4953937d68b298853214eaf2129
Fernando Jurado <ffjla@dtu.dk>
2023-10-05 15:26:26 +0200

    Training and test script for surrogate updated (Classification.)

--- tutorials/surrogate/test_classification.py
+++ tutorials/surrogate/test_classification.py
@@ -1,48 +1,26 @@
-# This script is used for the testing of the classification model
-import math
-import time
-import sys
 import os
+import sys
 import json
-import random
-import torch
-import numpy as np
 import argparse
-import matplotlib.pyplot as plt
-
-
+import numpy as np
+import torch
+import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-from torch import nn
 from torch.autograd import Variable
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
-
+# Constants
 SELF_PATH = os.path.dirname(os.path.abspath(__file__))
-# Go to the tutorial folder which is one folder up
 TUTORIALS_PATH = os.path.dirname(SELF_PATH)
-# Go to the results folder
 RESULTS_PATH = os.path.join(TUTORIALS_PATH, "results")
-# Folder to save the models
 MODELS_PATH = os.path.join(SELF_PATH, "models")
-
-# Lets create a dataloader
 BATCH_SIZE = 64
-
-# Neural network parameters
-INPUT_SIZE = 203
 HIDDEN_SIZE = 512
-OUTPUT_SIZE = 99
-NUM_CLUSTERS = 100
-LEARNING_RATE = 1e-46
 NUM_EPOCHS = 100
-PRINT_EVERY = 100
-PLOT_EVERY = 10000
 
 
 class NetworkDataset(Dataset):
-    # Data set that takes input and output data,
-    # and supports batching
     def __init__(self, x, y):
         self.x = torch.from_numpy(x.astype(np.float32))
         self.y = torch.from_numpy(y.astype(np.float32))
@@ -54,33 +32,27 @@ class NetworkDataset(Dataset):
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
 
-    def collate_fn(self, batch):
-        # Get the data
+    @staticmethod
+    def collate_fn(batch):
         x, y = zip(*batch)
-        # Stack the data
         x = torch.stack(x)
         y = torch.stack(y)
         return x, y
 
 
 class ClassificationModel(nn.Module):
-    # This model is used for classification
     def __init__(self, input_size, hidden_size, output_size):
         super(ClassificationModel, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-
-        # Lets define three layers
         self.fc1 = nn.Linear(self.input_size, self.hidden_size)
         self.fc2 = nn.Linear(self.hidden_size, self.hidden_size)
         self.fc3 = nn.Linear(self.hidden_size, self.output_size)
-        # Lets define the activation function
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        # Forward pass
         out = self.fc1(x)
         out = self.relu(out)
         out = self.fc2(out)
@@ -92,26 +64,20 @@ class ClassificationModel(nn.Module):
 
 def load_samples():
     samples = {}
-    for file in os.listdir("data_classification"):
-        # Ignore .DS_Store
+    data_dir = os.path.join(SELF_PATH, "data_classification")
+    for file in os.listdir(data_dir):
         if file == ".DS_Store":
             continue
-        # Load the data
-        with open(os.path.join("data_classification", file), "r") as f:
+        with open(os.path.join(data_dir, file), "r") as f:
             data = json.load(f)
-        # Get the name only which is after LEACH-CE-E_ and before .json
-        # Lets get the name which is simply the concatenation of what is inside the ''
-        name = file.split(".json")[0]
-        name = name.replace("(", "").replace(")", "").split(",")
-        name = [x.replace("'", "") for x in name]
-        name = [float(x) for x in name]
-        name = tuple(name)
+        # Remove single quotes and split by comma
+        name = tuple(float(x.replace("'", "")) for x in file.split(
+            ".json")[0].replace("(", "").replace(")", "").split(","))
         samples[name] = data
     return samples
 
 
 def get_all_samples(samples):
-    # Get the samples in the form of weights, current_membership, y_membership
     x = []
     y = []
     for key, sample in samples.items():
@@ -120,112 +86,54 @@ def get_all_samples(samples):
             y_data = sample[str(round)]['y_data']
             x.append(x_data)
             y.append(y_data)
-
     return x, y
 
 
 def closest_value(output):
-    # Convert tensors to numpy
-    output = np.array(output)
-
-    for i in range(len(output)):
-        # Get the closest value
-        closest = np.round(output[i]*100)
-        # Convert to integer
-        output[i] = closest.astype(int)
-    return output
+    return (output * 100).round().astype(int)
 
 
 def test_predicted_sample(encoder, x, y, output):
-    # print(f"X: {x}")
     y = encoder.inverse_transform(y)
     output = encoder.inverse_transform(output)
-
-    # For each element in the y, find the closest integer
     y = closest_value(y)
     output = closest_value(output)
-    # print(f"Y: {y}")
-    # print(f"Output: {output}")
-    # Convert to numpy
-    y = np.array(y)
-    output = np.array(output)
-    # Count the number of correct predictions
     correct = np.sum(y == output)
-    # Count the total number of predictions
     total = np.prod(y.shape)
-    # Get the index of the correct predictions
-    correct_index = np.where(y == output)
-    # print(f"correct_index {correct_index}")
-
-    # input(
-    #     f"Correct: {correct}, Total: {total}, Accuracy: {correct/total*100:.2f}%")
-
+    # Index where the values are equal
+    # index = np.where(y == output)
     return correct, total
 
 
 def test_predicted_batch(encoder, x, y, output):
-    # print shapes
-    # print(f"X: {x.shape}")
-    # print(f"Y: {y.shape}")
-    # print(f"Output: {output.shape}")
     accuracy = []
-
-    # Loop through the batch
     for i in range(len(output)):
-        # Get the ith output
         ith_output = output[i].reshape(1, -1)
-        # Get the ith y
         ith_y = y[i].reshape(1, -1)
-        # Get the ith x
         ith_x = x[i]
         correct, total = test_predicted_sample(
             encoder, ith_x, ith_y, ith_output)
-
-        accuracy.append(correct/total*100)
-
-    # Get the average accuracy
-    average_accuracy = np.mean(accuracy)
-
-    return average_accuracy
+        accuracy.append(correct/total * 100)
+    return np.mean(accuracy)
 
 
 def main(args):
     print(f"Loading model: {args.model}")
-
-    # Lets create a dataset
-
     samples = load_samples()
-
-    # Get all the samples
     x, y = get_all_samples(samples)
-
-    # Lets one hot encode the y and print the shape
     y = np.array(y)
     encoder = OneHotEncoder(sparse_output=False)
     y = encoder.fit_transform(y)
     x = np.array(x)
-
-    # Split the data into training and validation sets
-    X_train, X_test, y_train, y_test = train_test_split(
+    _, X_test, _, y_test = train_test_split(
         x, y, test_size=0.2, random_state=42, shuffle=False)
-
-    # Lets create a dataset
     test_dataset = NetworkDataset(X_test, y_test)
-
-    # Create a dataloader
     test_loader = DataLoader(
-        test_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=test_dataset.collate_fn)
-
-    # Load the model
+        test_dataset, batch_size=BATCH_SIZE, shuffle=False, collate_fn=test_dataset.collate_fn)
     model = ClassificationModel(
         test_dataset.x.shape[1], HIDDEN_SIZE, test_dataset.y.shape[1])
-
-    # Define the loss function
     criterion = nn.BCELoss()
-
     model.load_state_dict(torch.load(args.model))
-
-    # Lets test the model
     model.eval()
     losses = []
     avg_accuracy = []
@@ -233,29 +141,23 @@ def main(args):
         for batch_x, batch_y in test_loader:
             X = batch_x
             y = batch_y
-            # Get the output
             output = model(X)
-
-            if BATCH_SIZE == 1:
-                test_predicted_sample(encoder, x, y, output)
-                continue
-
             loss = criterion(output, y)
             losses.append(loss.item())
+            if BATCH_SIZE == 1:
+                correct, total = test_predicted_sample(encoder, x, y, output)
+                avg_accuracy.append(correct/total*100)
+                continue
             acc = test_predicted_batch(encoder, x, y, output)
             avg_accuracy.append(acc)
-
-    # Print the average loss
     print(f"Average Loss: {np.mean(losses)}")
-    # Print the average accuracy
     print(f"Average Accuracy: {np.mean(avg_accuracy)}")
 
 
 if __name__ == "__main__":
-    # argparse
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-m", "--model", help="The model to use for the testing", required=True
+        "-m", "--model", help="The model to use for testing", required=True
     )
     args = parser.parse_args()
     main(args)
