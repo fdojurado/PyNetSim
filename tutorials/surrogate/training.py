41c2831840607029d56a94500c0e32c5f3f11326
Fernando Jurado <ffjla@dtu.dk>
2023-10-08 09:10:13 +0200

    pyproject file modules updated.

--- tutorials/surrogate/training.py
+++ tutorials/surrogate/training.py
@@ -6,7 +6,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
