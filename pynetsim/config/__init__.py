e5e2cc4044a28362d5aaef9219fc088ec4b7a4cd
F. Fernando Jurado-Lasso <fdo.jurado@gmail.com>
2023-10-11 14:30:05 +0200

    Surrogate updated and running but not predicting well.

--- pynetsim/config/__init__.py
+++ pynetsim/config/__init__.py
@@ -7,6 +7,7 @@ from pynetsim.leach.rl.leach_rl_loss import LEACH_RL_LOSS
 from pynetsim.leach.rl.leach_hrl import LEACH_HRL
 from pynetsim.network.simple_model import Simple
 from pynetsim.network.extended_model import Extended
+from pynetsim.leach.surrogate.surrogate import SURROGATE
 from pynetsim.leach.leach_milp.leach_ce_d import LEACH_CE_D
 from pynetsim.leach.leach_milp.leach_ce_e import LEACH_CE_E
 from pynetsim.leach.leach_ce import LEACH_CE
@@ -36,7 +37,8 @@ PROTOCOLS = {
     "LEACH-R": LEACH_R,
     "LEACH-RT": LEACH_RT,
     "EC-LEACH": EC_LEACH,
-    "EE-LEACH": EE_LEACH
+    "EE-LEACH": EE_LEACH,
+    "Surrogate": SURROGATE
 }
 
 NETWORK_MODELS = {
--- pynetsim/leach/surrogate/surrogate.py
+++ pynetsim/leach/surrogate/surrogate.py
@@ -2,17 +2,15 @@ import numpy as np
 import pynetsim.common as common
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import torch
 
 from pynetsim.utils import PyNetSimLogger
-from pynetsim.leach.surrogate import ClassificationModel
 from rich.progress import Progress
-
-MODELS: dict = {
-    "classification": ClassificationModel
-}
+from pynetsim.leach.surrogate.model import SurrogateModel, NetworkDataset
+from torch.utils.data import DataLoader
 
 # -------------------- Create logger --------------------
-logger_utility = PyNetSimLogger(log_file="my_log.log")
+logger_utility = PyNetSimLogger(log_file="my_log.log", namespace=__name__)
 logger = logger_utility.get_logger()
 
 
@@ -23,11 +21,14 @@ class SURROGATE:
         self.net_model = net_model
         self.config = network.config
         self.network = network
-        self.model = self.config.protocol.model
+        self.largest_weight = self.config.surrogate.largest_weight
+        self.alpha = 0.5
+        self.beta = 2
+        self.gamma = 1.7
+        self.surrogate_model = SurrogateModel(config=self.config)
 
     def run(self):
         logger.info(f"Running {self.name}...")
-        logger.info(f"Neural network model: {self.model}")
         num_rounds = self.config.network.protocol.rounds
         plot_clusters_flag = False
 
@@ -45,15 +46,78 @@ class SURROGATE:
             self.run_with_plotting(
                 num_rounds)
 
+    def create_input_data(self):
+        # We need to get alpha, beta, gamma, and energy levels for all nodes
+        # We need to get alpha, beta, gamma for all nodes
+        weights = [self.alpha/self.largest_weight, self.beta /
+                   self.largest_weight, self.gamma/self.largest_weight]
+        w_np = np.array(weights)
+        logger.info(f"Weights: {w_np}, shape: {w_np.shape}")
+        # print all energy levels
+        # for node in self.network:
+        #     logger.info(f"Node {node.node_id} energy level: {node.remaining_energy}")
+        # We need to get energy levels for all nodes
+        energy_levels = np.array(
+            [node.remaining_energy for node in self.network if node.node_id != 1])
+        logger.info(
+            f"Energy levels: {energy_levels}, shape: {energy_levels.shape}")
+
+        # We need to get the current membership of all nodes, this involves in getting the
+        # cluster id of all nodes.
+        cluster_ids = np.array(
+            [0 if node.cluster_id is None else node.cluster_id for node in self.network])
+
+        logger.info(f"Cluster ids: {cluster_ids}, shape: {cluster_ids.shape}")
+        # Add a zero to the start of the cluster ids
+        cluster_ids = np.insert(cluster_ids, 0, 0)
+
+        logger.info(f"Cluster ids: {cluster_ids}, shape: {cluster_ids.shape}")
+
+        # Now we concatenate weights, and energy levels
+        input_data = np.concatenate((w_np, energy_levels))
+        logger.info(f"Input data: {input_data}, shape: {input_data.shape}")
+
+        return input_data, cluster_ids
+
+    def predict_cluster_assignments(self):
+        # Before we can predict the cluster assignments, we need to create the input data
+        # for the surrogate model.
+        numerical_data, categorical_data = self.create_input_data()
+        logger.info(f"Shapes of numerical and categorical data: {numerical_data.shape}, {categorical_data.shape}")
+        # Convert the numerical data to a tensor
+        numerical_data =torch.from_numpy(numerical_data.astype(np.float32))
+        # Convert the categorical data to a tensor
+        categorical_data =torch.from_numpy(categorical_data.astype(np.int64))
+        # Unsqueeze the numerical data
+        numerical_data = numerical_data.unsqueeze(0)
+        # Unsqueeze the categorical data
+        categorical_data = categorical_data.unsqueeze(0)
+        # print shapes
+        logger.info(f"Shapes of numerical and categorical data: {numerical_data.shape}, {categorical_data.shape}")
+        # Now we can predict the cluster assignments
+        model, _, _ = self.surrogate_model.get_model()
+        model.eval()
+        logger.info(
+            f"Shapes: {numerical_data.shape}, {categorical_data.shape}")
+        with torch.no_grad():
+                logger.info(
+                    f"Categorical data: {categorical_data}, shape: {categorical_data.shape}")
+                output = model(categorical_data=categorical_data,
+                               numerical_data=numerical_data)
+                logger.info(f"Output: {output}, shape: {output.shape}")
+                _, predicted_assignments = torch.max(output, dim=1)
+                logger.info(
+                    f"Predicted cluster assignments: {predicted_assignments}")
+        input("Press enter to continue...")
+
     def evaluate_round(self, round):
         round += 1
 
         for node in self.network:
             self.network.mark_as_non_cluster_head(node)
 
-        # self.choose_cluster_heads(potential_chs)
-        # self.network.create_clusters()
-        # self.net_model.dissipate_energy(round=round)
+        # Create cluster assignments predicted by the surrogate model
+        cluster_assignments = self.predict_cluster_assignments()
 
         return round
 
