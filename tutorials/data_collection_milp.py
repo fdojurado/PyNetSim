6f77cea031f64437503578405fd3dccbddf52eb3
F. Fernando Jurado-Lasso <fdo.jurado@gmail.com>
2023-10-21 08:07:28 +0200

    More samples added.

--- tutorials/data_collection_milp.py
+++ tutorials/data_collection_milp.py
@@ -3,6 +3,7 @@
 import pynetsim.leach.leach_milp as leach_milp
 import sys
 import os
+import random
 
 from pynetsim.leach.leach_milp.leach_ce_e import LEACH_CE_E
 from pynetsim.network.network import Network
@@ -36,30 +37,57 @@ def main():
 
     # Lets generate the all the combinations of weights.
 
-    max_alpha = 61
-    max_beta = 61
-    max_gamma = 61
+    # max_alpha = 61
+    # max_beta = 61
+    # max_gamma = 61
 
-    min_alpha = 1
-    min_beta = 1
-    min_gamma = 1
+    # min_alpha = 1
+    # min_beta = 1
+    # min_gamma = 1
 
-    step_size = 8
+    # step_size = 8
 
-    for alpha in range(min_alpha, max_alpha, step_size):
-        alpha_weight = alpha / 10
-        for beta in range(min_beta, max_beta, step_size):
-            beta_weight = beta / 10
-            for gamma in range(min_gamma, max_gamma, step_size):
-                gamma_weight = gamma / 10
-                network_copy, network_model_copy = leach_milp.copy_network(
-                    network, network_model)
-                # Lets create the object of the LEACH-CE-E protocol.
-                leach_ce_e = LEACH_CE_E(network_copy, network_model_copy,
-                                        alpha=alpha_weight, beta=beta_weight, gamma=gamma_weight)
-                network_copy.stats.name = f"LEACH-CE-E_{alpha_weight}_{beta_weight}_{gamma_weight}"
-                # Lets run the protocol.
-                leach_ce_e.run()
+    # for alpha in range(min_alpha, max_alpha, step_size):
+    #     alpha_weight = alpha / 10
+    #     for beta in range(min_beta, max_beta, step_size):
+    #         beta_weight = beta / 10
+    #         for gamma in range(min_gamma, max_gamma, step_size):
+    #             gamma_weight = gamma / 10
+    #             network_copy, network_model_copy = leach_milp.copy_network(
+    #                 network, network_model)
+    #             # Lets create the object of the LEACH-CE-E protocol.
+    #             leach_ce_e = LEACH_CE_E(network_copy, network_model_copy,
+    #                                     alpha=alpha_weight, beta=beta_weight, gamma=gamma_weight)
+    #             network_copy.stats.name = f"LEACH-CE-E_{alpha_weight}_{beta_weight}_{gamma_weight}"
+    #             # Lets run the protocol.
+    #             leach_ce_e.run()
+
+    # Define the range of numbers
+    min_value = 0
+    max_value = 10
+
+    # Define the number of combinations you want
+    num_combinations = 200
+
+    # Generate random combinations
+    combinations = []
+
+    for _ in range(num_combinations):
+        combination = [random.uniform(min_value, max_value) for _ in range(3)]
+        combinations.append(combination)
+
+    print(f"Shape of combinations: {len(combinations)}")
+
+    for combo in combinations:
+        print(f"Combo: {combo}")
+        network_copy, network_model_copy = leach_milp.copy_network(
+            network, network_model)
+        # Lets create the object of the LEACH-CE-E protocol.
+        leach_ce_e = LEACH_CE_E(network_copy, network_model_copy,
+                                alpha=combo[0], beta=combo[1], gamma=combo[2])
+        network_copy.stats.name = f"LEACH-CE-E_{combo[0]}_{combo[1]}_{combo[2]}"
+        # Lets run the protocol.
+        leach_ce_e.run()
 
 
 if __name__ == "__main__":
