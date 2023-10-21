4ec195f9dfce150be1c114efc9992aa4d360c3ab
F. Fernando Jurado-Lasso <fdo.jurado@gmail.com>
2023-10-21 08:43:14 +0200

    Random energy set at each combination.

--- tutorials/data_collection_milp.py
+++ tutorials/data_collection_milp.py
@@ -78,15 +78,14 @@ def main():
 
     print(f"Shape of combinations: {len(combinations)}")
 
-    # Generate a random initial energy for each node.
-    for node in network:
-        random_energy = random.uniform(0, 2)
-        node.remaining_energy = random_energy
-
     for combo in combinations:
         print(f"Combo: {combo}")
         network_copy, network_model_copy = leach_milp.copy_network(
             network, network_model)
+        # Generate a random initial energy for each node.
+        for node in network_copy:
+            random_energy = random.uniform(0, 1)
+            node.remaining_energy = random_energy
         # Lets create the object of the LEACH-CE-E protocol.
         leach_ce_e = LEACH_CE_E(network_copy, network_model_copy,
                                 alpha=combo[0], beta=combo[1], gamma=combo[2])
